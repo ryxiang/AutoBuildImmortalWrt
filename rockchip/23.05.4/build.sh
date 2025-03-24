02041f9ae043b463d784e2a4e27806972f1e9bd5
ryxiang <52651563+ryxiang@users.noreply.github.com>
2025-03-24 23:21:37 +0800

    Update build.sh

--- rockchip/23.05.4/build.sh
+++ rockchip/23.05.4/build.sh
@@ -34,7 +34,6 @@ PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 # 增加几个必备组件 方便用户安装iStore
